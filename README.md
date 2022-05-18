6554fac7608387098be849b88e2224e1dd34ad23
wasinkp <73060136+SinsamutQ@users.noreply.github.com>
2022-05-18 17:11:32 +0700

    add 5 command

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Q1-RESEARCH-Linebot
 Researching . . . 🧪
+
+## starter
+EP.15 What’s New - สร้าง LINE Chatbot ด้วย Node.js !!
+- https://www.youtube.com/watch?v=PY5KlS-c03Y
+
+## concept
+### show
+<!-- xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx -->
+if
+    req > สมาชิก 
+    res > ข้อมูลสมาชิก
+if
+    req > คะแนน
+    res > คะแนนที่มีทั้งหมด
+if
+    req > โปรโมชั่น
+    res > โปรโมชั่นทั้งหมด
+if
+    req > ขอเมนู
+    res > เมนูทั้งหมด
+<!-- xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx -->
+### edit
+<!-- xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx -->
+if
+    req > สั่งเมนู A
+    res > เพิ่มเมนู A เข้า DB , แสดงรายละเอียด
+if
+    req > ยกเลิกเมนู A
+    res > ยกเลิกเมนู A เข้า DB
+<!-- xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx -->
+
+
+
+## flow
+### 🟢 doing 
+if
+    req > type = todo 
+    res > todo
+if
+    req > type = user
+    res > user
--- second-bot/index.js
+++ second-bot/index.js
@@ -16,7 +16,7 @@ const client = new line.Client(lineConfig);
 app.post("/webhook", line.middleware(lineConfig), async (req, res) => {
     try {
         const events = req.body.events;
-        console.log("event=>>>>", events);
+        console.log("event >>>>", events);
         return events.length > 0
             ? await events.map((item) => handleEvent(item))
             : res.status(200).send("OK");
@@ -25,6 +25,160 @@ app.post("/webhook", line.middleware(lineConfig), async (req, res) => {
     }
 });
 
+// xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx
+
+// const handleEvent = async (event) => {
+//     if (event.type !== 'message' || event.message.type !== 'text') {
+//         return null;
+//     }
+//     else if (event.type === 'message') {
+//         try {
+//             const { data } = await axios.get(
+//                 `https://jsonplaceholder.typicode.com/todos/${event.message.text}`
+//             ); //use data destructuring to get data from the promise object
+//             console.log("Data -> ", data);
+//             messageAll = [
+//                 { type: "text", text: "userId : " + data.userId },
+//                 { type: "text", text: "id : " + data.id },
+//                 { type: "text", text: "title : " + data.title },
+//                 { type: "text", text: "completed : " + data.completed },
+//             ];
+//             console.log("messageAll -> ", messageAll);
+
+//             return client.replyMessage(event.replyToken, messageAll);
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }
+// };
+
+//
+
+const handleEvent = async (event) => {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return null;
+    }
+    else if (event.type === 'message') {
+        if (event.message.text === "ขอรายละเอียด") {
+            messageAll = [
+                // { type: "text", text: " ⚡️ Todo \n ⚡️ User \n ⚡️ Point \n ⚡️ Promotion \n ⚡️ ..."},
+                { type: "text", text: 
+                    " 🎃 all command " +
+                    "\n cmd : ขอรายละเอียด " +
+                    "\n cmd : Todo " +
+                    "\n cmd : User " +
+                    "\n cmd : Point " + 
+                    "\n cmd : Promotion" +
+                    "\n " +
+                    "\n 🎃 doing " +
+                    "\n 1 " +
+                    "\n 2 "  
+                }
+            ];
+            console.log("messageAll -> ", messageAll);
+            return client.replyMessage(event.replyToken, messageAll);
+        }
+
+        // TODO
+        else if (event.message.text === "Todo") {
+            try {
+                const { data } = await axios.get(
+                    `https://jsonplaceholder.typicode.com/todos/1`
+                ); //use data destructuring to get data from the promise object
+                console.log("Data -> ", data);
+                messageAll = [
+                    { type: "text", text: 
+                        " userId : " + data.userId + 
+                        "\n id : " + data.id + 
+                        "\n title : " + data.title + 
+                        "\n completed : " + data.completed 
+                    },
+                ];
+                console.log("messageAll -> ", messageAll);
+    
+                return client.replyMessage(event.replyToken, messageAll);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        // USER
+        else if (event.message.text === "User") {
+            try {
+                const { data } = await axios.get(
+                    `https://jsonplaceholder.typicode.com/users/1`
+                ); //use data destructuring to get data from the promise object
+                console.log("Data -> ", data);
+                messageAll = [
+                    { type: "text", text: 
+                        " id : " + data.id  +
+                        "\n name : " + data.name +
+                        "\n username : " + data.username +
+                        "\n email : " + data.email +
+                        "\n phone : " + data.phone
+                    },
+                ];
+                console.log("messageAll -> ", messageAll.text);
+                return client.replyMessage(event.replyToken, messageAll);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        // POINT
+        else if (event.message.text === "Point") {
+            try {
+                // const { data } = await axios.get(
+                //     `{mock}https://localhost/point`
+                // ); 
+                // console.log("Data -> ", data);
+                messageAll = [
+                    { type: "text", text: 
+                        " You have 999,999 Point \n ซื้อของเพื่อสะสมเเต้มเพิ่ม 😆" 
+                    },
+                ];
+                console.log("messageAll -> ", messageAll);
+                return client.replyMessage(event.replyToken, messageAll);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        // PROMOTION
+        else if (event.message.text === "Promotion") {
+            try {
+                // const { data } = await axios.get(
+                //     `{mock}https://localhost/PROMOTION`
+                // ); 
+                // console.log("Data -> ", data);
+                messageAll = [
+                    { type: "text", text: 
+                        "promotion วันนี้ มื้อนี้อย่างคุ้ม! ปรากฏการณ์ลดเยอะกว่า รับส่วนลดสูงสุด 560 บาท " 
+                    },
+                ];
+                console.log("messageAll -> ", messageAll);
+                return client.replyMessage(event.replyToken, messageAll);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+};
+
+
+// xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx
+
+
+app.listen(4000, () => {
+    console.log("listening on 4000");
+});
+
+
+
+
+
+
+// 🧪 success
 // const handleEvent = async (event) => {
 //     if(event.type !== 'message' || event.message.type !== 'text'){
 //         return null;
@@ -34,65 +188,31 @@ app.post("/webhook", line.middleware(lineConfig), async (req, res) => {
 //     }
 // }
 
-const handleEventxxx = async (event) => {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return null;
-    }
-    else if (event.type === 'message') {
-        const { data } = await axios.get(`https://${env.RAPID_URL}/words/${event.message.text}/synonyms`, {
-        headers: {
-            'x-rapidapi-host': env.RAPID_URL,
-            'x-rapidapi-key': env.RAPID_KEY
-        }
-    })
-        console.log('Data-> ', data)
-        const { synonyms } = data
-        let str = ''
-        synonyms.forEach((result, i) => {
-            // str += synonyms.length - 1 !== i ? `${result}\n` : result
-            if (synonyms.length - 1 !== i){
-                str += `${result}\n`
-            } else {
-                str += result
-            }
-        })
-        console.log("STR =>>>>", str)
-        return client.replyMessage(event.replyToken, { type: 'text', text: str });
-    }
-}
 
-// xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx xxxxxxxxxx
-
-const handleEvent = async (event) => {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return null;
-    }
-    else if (event.type === 'message'){
-        try {
-            const { data } = await axios.get(
-                `https://jsonplaceholder.typicode.com/todos/1`
-            ); //use data destructuring to get data from the promise object
-            console.log("Data -> ", data);
-            //   const { synonyms } = data
-            //   let str = ''
-            //   synonyms.forEach((result, i) => {
-            //     str += synonyms.length - 1 !== i ? `${result}\n` : result
-            //   })
-            messageAll = [
-                { type: "text", text: "userId : " + data.userId },
-                { type: "text", text: "id : " + data.id },
-                { type: "text", text: "title : " + data.title },
-                { type: "text", text: "completed : " + data.completed },
-            ];
-            console.log("messageAll -> ", messageAll);
-    
-            return client.replyMessage(event.replyToken, messageAll);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-};
-
-app.listen(4000, () => {
-    console.log("listening on 4000");
-});
+// 🧪 example
+// const handleEvent = async (event) => {
+//     if (event.type !== 'message' || event.message.type !== 'text') {
+//         return null;
+//     }
+//     else if (event.type === 'message') {
+//         const { data } = await axios.get(`https://${env.RAPID_URL}/words/${event.message.text}/synonyms`, {
+//         headers: {
+//             'x-rapidapi-host': env.RAPID_URL,
+//             'x-rapidapi-key': env.RAPID_KEY
+//         }
+//     })
+//         console.log('Data-> ', data)
+//         const { synonyms } = data
+//         let str = ''
+//         synonyms.forEach((result, i) => {
+//             // str += synonyms.length - 1 !== i ? `${result}\n` : result
+//             if (synonyms.length - 1 !== i){
+//                 str += `${result}\n`
+//             } else {
+//                 str += result
+//             }
+//         })
+//         console.log("STR =>>>>", str)
+//         return client.replyMessage(event.replyToken, { type: 'text', text: str });
+//     }
+// }
